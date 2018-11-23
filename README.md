fa3920fe3e0ad3ab31fcfe93ac1dec63e7b07653
Brian Mitchell <brian.mitchell@chrobinson.com>
2018-11-23 14:51:03 -0600

    Remove jQuery, drop support for <IE10, upgrade grunt and jshint

--- README.md
+++ README.md
@@ -14,7 +14,31 @@ bundle install
 
 ```sh
 bundle exec jekyll serve
-# include --watch to re-build on changes or --livereload to re-build and refresh on changes
+# include --livereload to re-build and refresh on changes
+bundle exec jekyll serve --livereload
 ```
 
 The site will now be served from `http://localhost:4000`
+
+### Bundling JavaScript Files
+
+When the site is build by Jekyll, only `js/main.js` is actually loaded into
+the browser. Grunt bundles and minifies all of the scripts and outputs them
+into `js/main.js`. This site supports IE10-11, and script code should be kept
+to a minimum, so scripts must be ECMAScript 5.1 compliant.
+
+If you are modifying any JavaScript files, you must install node to be able
+to bundle and minify the scripts. To install the dependencies, run
+
+```sh
+npm install
+```
+
+Then, to build, run
+
+```sh
+# rebuild the scripts and refresh the page on save
+npm run start
+# build the scripts and exit
+npm run build
+```
--- js/_main.js
+++ js/_main.js
@@ -1,20 +1,47 @@
-// Off Canvas Sliding
-$(document).ready(function(){
-  // Menu button click
-  $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-    // $('#js-body').toggleClass('no-scroll');
-    $('#js-menu, #js-menu-screen').toggleClass('is-visible');
-    $('#js-menu-trigger').toggleClass('slide close');
-    // $('#masthead, #page-wrapper').toggleClass('slide');
-    e.preventDefault();
+(function() {
+  'use strict';
+  var addHandler = function(el, events, handler) {
+    if (!el) {
+      return;
+    }
+    for (var i = 0; i < events.length; i += 1) {
+      el.addEventListener(events[i], handler);
+    }
+  };
+
+  var jsMenuInit = function() {
+    // Off Canvas Sliding
+    // Menu button click
+    var jsMenuTrigger = document.getElementById('js-menu-trigger');
+    var jsMenuScreen = document.getElementById('js-menu-screen');
+    var jsMenu = document.getElementById('js-menu');
+
+    var handler = function(e) {
+      jsMenu.classList.toggle('is-visible');
+      jsMenuScreen.classList.toggle('is-visible');
+      jsMenuTrigger.classList.toggle('slide');
+      jsMenuTrigger.classList.toggle('close');
+      e.preventDefault();
+    };
+
+    addHandler(jsMenuTrigger, ['click', 'touchstart'], handler);
+    addHandler(jsMenuScreen, ['click', 'touchstart'], handler);
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    jsMenuInit();
+
+    fluidvids.init({
+      selector: ['iframe', 'object'],
+      players: ['www.youtube.com', 'player.vimeo.com']
+    });
+
+    // Table of Contents title. Change text to localize
+    var toc = document.getElementById('markdown-toc');
+    if (toc) {
+      var overview = document.createElement('li');
+      overview.innerHTML = '<h6>{{ site.data.messages.locales[site.locale].overview }}</h6>';
+      toc.insertBefore(overview, toc.firstChild);
+    }
   });
-});
-
-// FitVids
-$(document).ready(function(){
-	// Target your .container, .wrapper, .post, etc.
-	$("#main").fitVids();
-});
-
-// Table of Contents title. Change text to localize
-$("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
+})();
